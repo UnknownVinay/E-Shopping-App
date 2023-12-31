e8190d9c097e6aa47adef1b3091671ffb00f0a7b
vinay jadhav <vinayjadhav619@gmail.com>
2023-12-31 14:42:21 +0530

    added backend

---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
+
+const ProductPage = () => {
+  const [product, setProduct] = useState([]);
+  const { id: productId } = useParams();
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/product/${productId}`);
+      setProduct(data);
+      console.log(data);
+    };
+    fetchProducts();
+  }, [productId]);
+
+  // const product = products.find((p) => {
+  //   return p._id === productId;
+  // });
+  // console.log(product);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>
+              <strong>Description</strong>: {product.description}
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductPage;
