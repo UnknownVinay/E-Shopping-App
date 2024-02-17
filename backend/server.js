81dc0c34da7b7429424b3ac09a7ec46cc0b71420
vinay jadhav <vinayjadhav619@gmail.com>
2024-02-17 22:31:46 +0530

    added build script

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middlerware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 app.use("/api/products", productRoutes);
 
 app.use("/api/users", userRoutes);
@@ -35,6 +32,20 @@ app.use("/api/uploads", uploadRoutes);
 //to make uploads folder static
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  //setting frontend build folder as static
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //all other routes are redirected to index.html
+  app.use("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
